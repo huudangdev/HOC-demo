effb8a68b3817a83277e4c7d7e33b8a2d71463fb
huudangdev <nguyentranhuudang.blog@gmail.com>
2019-08-13 17:51:28 +0700

    Updates

---
+++ precache-manifest.bf6e576621b688cf034b2d3f7147e368.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76b510605a95b2166f93ad5514c84fc",
+    "url": "/HOC-demo/index.html"
+  },
+  {
+    "revision": "adaae4f6768c49bcebc4",
+    "url": "/HOC-demo/static/css/main.9d2b0661.chunk.css"
+  },
+  {
+    "revision": "f3279d3cc6be445b8fa3",
+    "url": "/HOC-demo/static/js/2.4d0222a8.chunk.js"
+  },
+  {
+    "revision": "adaae4f6768c49bcebc4",
+    "url": "/HOC-demo/static/js/main.9f0aaa2a.chunk.js"
+  },
+  {
+    "revision": "5b069a9e7ff17f958d0f",
+    "url": "/HOC-demo/static/js/runtime~main.813da4ce.js"
+  }
+]);
